3fb1f3650e31d53e64e0a65dbdd4a189ca37360e
none <none>
2026-03-06 09:36:10 +0530

    puneeth Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to " + appName);
+        System.out.println("   Hotel Booking Management System");
+        System.out.println("   Version: " + version);
+        System.out.println("=================================");
+        System.out.println("Application started successfully!");
+    }
+}
